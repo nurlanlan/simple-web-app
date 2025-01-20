24af591e8b15629efd33e53cecb4d5336c16a600
nurlanlan <nurahmad004@gmail.com>
2025-01-20 19:49:02 +0400

    version 1

--- src/main/java/tech/coeus/simpleWebApp/HomeController.java
+++ src/main/java/tech/coeus/simpleWebApp/HomeController.java
@@ -2,11 +2,13 @@ package tech.coeus.simpleWebApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @RequestMapping("/")
+    @ResponseBody
     public String greet(){
         return "Hello World";
     }
