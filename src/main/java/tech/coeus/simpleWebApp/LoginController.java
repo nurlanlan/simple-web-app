b6ef8d2b32c0023ba992618050a9a78dc362b20f
nurlanlan <nurahmad004@gmail.com>
2025-01-20 19:55:14 +0400

    multi controller version

---
+++ src/main/java/tech/coeus/simpleWebApp/LoginController.java
@@ -0,0 +1,13 @@
+package tech.coeus.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "Hello World Login page";
+    }
+}
